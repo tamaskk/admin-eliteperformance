1514d2d4816dd452fffc37314736fe8056d7c174
Tamas Krisztian Kalman <kalman.tamaskrisztian@gmail.com>
2024-10-12 15:45:13 +0200

    fix

--- components/blogs.tsx
+++ components/blogs.tsx
@@ -18,7 +18,6 @@ const Blogs = () => {
     try {
       const blogsResponse = await getBlogs();
       setBlogs(blogsResponse.data);
-      console.log(blogsResponse.data);
     } catch (error) {
       console.error("Failed to fetch blogs:", error);
     } finally {
@@ -36,9 +35,9 @@ const Blogs = () => {
 
   useEffect(() => {
     if (status === "loading") return;
-    
+
     if (!session) {
-      router.push('/login');
+      router.push("/login");
     }
   }, [session, status, router]);
 
@@ -46,19 +45,31 @@ const Blogs = () => {
     return <div>Loading...</div>;
   }
 
-
   const changeBlog = async (id: string) => {
-    console.log('1')
-    // router.push(`/blog-posts/${id}`);
-    window.location.href = `/blog-posts/${id}`;
-    console.log('2')
+    // Ensure you are getting the correct id
+    if (!id) {
+      console.error("No valid ID provided");
+      return;
+    }
+  
+    console.log("Navigating to blog with ID:", id);
+  
+    try {
+      // Use router.push to navigate programmatically
+      await router.push(`/blog-posts/${id}`);
+    } catch (error) {
+      console.error("Failed to navigate:", error);
+    }
   };
+  
 
   const deleteBlog = async (id: string) => {
-    const confirmDelete = confirm("Biztosan törölni szeretnéd ezt a blog posztot?");
+    const confirmDelete = confirm(
+      "Biztosan törölni szeretnéd ezt a blog posztot?"
+    );
     if (!confirmDelete) return;
     toast.loading("Törlés folyamatban...");
-  
+
     try {
       await deleteBlogItem(id);
       // Ensure blogs is not undefined
@@ -73,7 +84,11 @@ const Blogs = () => {
   };
 
   const watchBlog = async (id: string) => {
-      router.push(`https://eliteperformance.hu/blog-posts/${blogs?.find((blog) => blog.id === id)?.title.replace(/ /g, "-")}`);
+    router.push(
+      `https://eliteperformance.hu/blog-posts/${blogs
+        ?.find((blog) => blog.id === id)
+        ?.title.replace(/ /g, "-")}`
+    );
   };
 
   const createBlog = async () => {
@@ -97,62 +112,64 @@ const Blogs = () => {
         <h1 className="font-bold text-3xl">Blog posztok</h1>
         <div className="flex flex-row items-center justify-center gap-2">
           <button
-                    className="bg-black text-white px-4 py-2 rounded-xl hover:bg-[#000001]"
-          onClick={() => fetchBlogs()}
+            className="bg-black text-white px-4 py-2 rounded-xl hover:bg-[#000001]"
+            onClick={() => fetchBlogs()}
           >
             Posztok lekérése
           </button>
-        <button
-          onClick={() => createBlog()}
-          className="bg-black text-white px-4 py-2 rounded-xl hover:bg-[#000001]"
+          <button
+            onClick={() => createBlog()}
+            className="bg-black text-white px-4 py-2 rounded-xl hover:bg-[#000001]"
           >
-          Új poszt
-        </button>
-          </div>
+            Új poszt
+          </button>
+        </div>
       </div>
-      {
-        loading && (
-          <div className="w-full h-screen flex justify-center items-center">
-            <p className="text-2xl text-black">Betöltés...</p>
-          </div>
-        )
-      }
+      {loading && (
+        <div className="w-full h-screen flex justify-center items-center">
+          <p className="text-2xl text-black">Betöltés...</p>
+        </div>
+      )}
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-10">
-        {
-          blogs && blogs.length === 0 && !loading && (
-            <div className="w-full h-full flex justify-center items-center">
-              <p className="text-2xl">Nincs megjeleníthető blog poszt</p>
-            </div>
-          )
-        }
-        {blogs && blogs.map((post) => (
-          <div key={post.id} className="flex flex-col shadow-lg p-4 rounded-xl">
-            <img
-              src={post.coverImage}
-              alt={post.title}
-              className="rounded-lg"
-            />
-            <h2 className="font-bold text-xl mt-4 text-center">{post.title}</h2>
-            <button
-              onClick={() => watchBlog(post.id.toString())}
-              className="w-full h-auto py-2 bg-yellow-500 rounded-xl my-4 hover:bg-yellow-600 transition-all duration-300"
-            >
-              Megtekintés
-            </button>
-            <button
-              onClick={() => changeBlog(post._id ? post._id.toString() : "")}
-              className="w-full h-auto py-2 bg-green-500 rounded-xl mb-4 hover:bg-green-600 transition-all duration-300"
-            >
-              Szerkesztés
-            </button>
-            <button
-              onClick={() => deleteBlog(post.id.toString())}
-              className="w-full h-auto py-2 bg-red-500 rounded-xl mb-4 hover:bg-red-600 transition-all duration-300"
-            >
-              Törlés
-            </button>
+        {blogs && blogs.length === 0 && !loading && (
+          <div className="w-full h-full flex justify-center items-center">
+            <p className="text-2xl">Nincs megjeleníthető blog poszt</p>
           </div>
-        ))}
+        )}
+        {blogs &&
+          blogs.map((post) => (
+            <div
+              key={post.id}
+              className="flex flex-col shadow-lg p-4 rounded-xl"
+            >
+              <img
+                src={post.coverImage}
+                alt={post.title}
+                className="rounded-lg"
+              />
+              <h2 className="font-bold text-xl mt-4 text-center">
+                {post.title}
+              </h2>
+              <button
+                onClick={() => watchBlog(post.id.toString())}
+                className="w-full h-auto py-2 bg-yellow-500 rounded-xl my-4 hover:bg-yellow-600 transition-all duration-300"
+              >
+                Megtekintés
+              </button>
+              <button
+                onClick={() => changeBlog(post._id ? post._id.toString() : "")}
+                className="w-full h-auto py-2 bg-green-500 rounded-xl mb-4 hover:bg-green-600 transition-all duration-300"
+              >
+                Szerkesztés
+              </button>
+              <button
+                onClick={() => deleteBlog(post.id.toString())}
+                className="w-full h-auto py-2 bg-red-500 rounded-xl mb-4 hover:bg-red-600 transition-all duration-300"
+              >
+                Törlés
+              </button>
+            </div>
+          ))}
       </div>
     </div>
   );
